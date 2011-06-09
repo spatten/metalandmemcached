7da57e52dfa3bf33350809cb96158d9846ba13a3
Scott Patten <scott@ruboss.com>
2011-06-08 21:09:19 -0700

    fresh generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metaltest::Application.config.secret_token = 'f8a02b8377c7948fa8f85edd7f411fe7c63cc51e57f4f5b144815869706403f49b55c5b8c55da2bea61e5d90d044630faa39acf02c22d24bd1a1cc4d13ac0749'
